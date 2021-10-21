60b6de2702271c0334ee5b492fccc6a03524e0ec
thinkbase <thinkbase.net@gmail.com>
2021-10-21 22:57:56 +0800

    feature: Use springboot to run ftp server

---
+++ bizobj-ftp-server/src/main/java/org/bizobj/ftpserver/bizobjftpserver/BizobjFtpServerApplication.java
@@ -0,0 +1,13 @@
+package org.bizobj.ftpserver.bizobjftpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizobjFtpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizobjFtpServerApplication.class, args);
+	}
+
+}
---
+++ bizobj-ftp-server/src/test/java/org/bizobj/ftpserver/bizobjftpserver/BizobjFtpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bizobj.ftpserver.bizobjftpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizobjFtpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
